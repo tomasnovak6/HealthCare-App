9e27a4362ff16b925e9e6f383b077472962e9b5d
Tomas Novak <novak2@komix.cz>
2020-10-05 22:39:33 +0200

    Main components

--- healthcare-app-fe/src/app/app.module.ts
+++ healthcare-app-fe/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { QuestionareComponent } from './questionare/questionare.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    StatisticsComponent,
+    QuestionareComponent
   ],
   imports: [
     BrowserModule,
